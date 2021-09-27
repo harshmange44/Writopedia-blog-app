a8cbe7eb2dfbe0908881a075dcdb4946dde347ea
Harsh Mange <harshmange44@gmail.com>
2021-09-27 09:48:56 +0530

    .

--- models/Token.js
+++ models/Token.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const tokenSchema = new mongoose.Schema({
     _userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
     token: { type: String, required: true },
-    expireAt: { type: Date, default: Date.now, index: { expires: 86400000 } }
+    expire_at: { type: Date, default: Date.now, expires: 90 } //86400000
 });
 
 module.exports = mongoose.model("Token", tokenSchema);
